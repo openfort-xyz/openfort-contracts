b628ac5a79f3302c0b6c7ee5ba19fd106679c545
Eloi Manuel <eloi.manvie@gmail.com>
2023-05-02 13:38:40 +0200

    Added dotenv to Hardhat

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,6 +2,9 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@nomicfoundation/hardhat-foundry";
 
+import { config as dotEnvConfig } from "dotenv";
+dotEnvConfig();
+
 const config: HardhatUserConfig = {
   solidity: "0.8.18",
 };
