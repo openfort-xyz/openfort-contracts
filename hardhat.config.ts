32ccc6afcb684165bc2507e1e358bc6dc8531e00
Eloi Manuel <eloi.manvie@gmail.com>
2023-05-02 14:16:56 +0200

    Updated hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,12 +1,32 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@nomicfoundation/hardhat-foundry";
-
 import { config as dotEnvConfig } from "dotenv";
-dotEnvConfig();
+import { resolve } from "path";
+import "./tasks/accounts";
+
+const dotenvConfigPath: string = process.env.DOTENV_CONFIG_PATH || "./.env";
+dotEnvConfig({ path: resolve(__dirname, dotenvConfigPath) });
+
+// Ensure that we have all the environment variables we need.
+const mnemonic: string | undefined = process.env.MNEMONIC;
+if (!mnemonic) {
+  throw new Error("Please set your MNEMONIC in a .env file");
+}
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.18",
+  defaultNetwork: "hardhat",
+  solidity: {
+    version: "0.8.17",
+    settings: {
+      optimizer: { enabled: true },
+    },
+  },
+  networks: {
+    hardhat: {
+      chainId: 31337,
+    },
+  }
 };
 
 export default config;
