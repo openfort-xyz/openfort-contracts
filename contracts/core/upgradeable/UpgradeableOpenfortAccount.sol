487a3dfec4d11b5ed8dec67efa0003c3d827570e
Eloi Manuel <eloi.manvie@gmail.com>
2023-05-11 19:48:41 +0200

    Adding tests to UpgradeableOpenfort

--- contracts/core/upgradeable/UpgradeableOpenfortAccount.sol
+++ contracts/core/upgradeable/UpgradeableOpenfortAccount.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.12;
 
 // Base account contract to inherit from
-import {BaseOpenfortAccount, IEntryPoint} from "../BaseOpenfortAccount.sol";
+import {BaseUpgradeableOpenfortAccount, IEntryPoint} from "../BaseUpgradeableOpenfortAccount.sol";
 import {UUPSUpgradeable} from "@openzeppelin/contracts/proxy/utils/UUPSUpgradeable.sol";
 
 /**
@@ -10,10 +10,10 @@ import {UUPSUpgradeable} from "@openzeppelin/contracts/proxy/utils/UUPSUpgradeab
   * @author Eloi<eloi@openfort.xyz>
   * @notice Minimal smart contract wallet with session keys following the ERC-4337 standard.
   * It inherits from:
-  *  - BaseOpenfortAccount
+  *  - BaseUpgradeableOpenfortAccount
   */
-contract UpgradeableOpenfortAccount is BaseOpenfortAccount, UUPSUpgradeable {
-    constructor(IEntryPoint _entrypoint, address _factory) BaseOpenfortAccount(_entrypoint,  _factory){}
+contract UpgradeableOpenfortAccount is BaseUpgradeableOpenfortAccount, UUPSUpgradeable {
+    constructor(IEntryPoint _entrypoint, address _factory) BaseUpgradeableOpenfortAccount(_entrypoint,  _factory){}
     
     function _authorizeUpgrade(address) internal override onlyOwner {}
 }
