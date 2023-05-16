564fafa18bf22b0528cf62e65e3427e6ae96217e
Eloi Manuel <eloi.manvie@gmail.com>
2023-05-16 17:08:54 +0200

    OpenfortPaymaster testing

--- test/foundry/paymaster/OpenfortPaymasterTest.t.sol
+++ test/foundry/paymaster/OpenfortPaymasterTest.t.sol
@@ -27,7 +27,91 @@ contract OpenfortPaymasterTest is Test {
 
     address private accountAdmin;
     uint256 private accountAdminPKey;
+
+    address payable private beneficiary = payable(makeAddr("beneficiary"));
     
+    event AccountCreated(address indexed account, address indexed accountAdmin);
+
+    /*
+     * Auxiliary function to generate a userOP
+     */
+    function _setupUserOp(
+        address sender,
+        uint256 _signerPKey,
+        bytes memory _initCode,
+        bytes memory _callDataForEntrypoint
+    ) internal returns (UserOperation[] memory ops) {
+        uint256 nonce = entryPoint.getNonce(sender, 0);
+
+        // Get user op fields
+        UserOperation memory op = UserOperation({
+            sender: sender,
+            nonce: nonce,
+            initCode: _initCode,
+            callData: _callDataForEntrypoint,
+            callGasLimit: 500_000,
+            verificationGasLimit: 500_000,
+            preVerificationGas: 500_000,
+            maxFeePerGas: 0,
+            maxPriorityFeePerGas: 0,
+            paymasterAndData: bytes(""),
+            signature: bytes("")
+        });
+
+        // Sign UserOp
+        bytes32 opHash = EntryPoint(entryPoint).getUserOpHash(op);
+        bytes32 msgHash = ECDSA.toEthSignedMessageHash(opHash);
+
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(_signerPKey, msgHash);
+        bytes memory userOpSignature = abi.encodePacked(r, s, v);
+
+        address recoveredSigner = ECDSA.recover(msgHash, v, r, s);
+        address expectedSigner = vm.addr(_signerPKey);
+        assertEq(recoveredSigner, expectedSigner);
+
+        op.signature = userOpSignature;
+
+        // Store UserOp
+        ops = new UserOperation[](1);
+        ops[0] = op;
+    }
+
+    /* 
+     * Auxiliary function to generate a userOP using the execute()
+     * from the account
+     */
+    function _setupUserOpExecute(
+        address sender,
+        uint256 _signerPKey,
+        bytes memory _initCode,
+        address _target,
+        uint256 _value,
+        bytes memory _callData
+    ) internal returns (UserOperation[] memory) {
+        bytes memory callDataForEntrypoint =
+            abi.encodeWithSignature("execute(address,uint256,bytes)", _target, _value, _callData);
+
+        return _setupUserOp(sender, _signerPKey, _initCode, callDataForEntrypoint);
+    }
+
+    /* 
+     * Auxiliary function to generate a userOP using the executeBatch()
+     * from the account
+     */
+    function _setupUserOpExecuteBatch(
+        address sender,
+        uint256 _signerPKey,
+        bytes memory _initCode,
+        address[] memory _target,
+        uint256[] memory _value,
+        bytes[] memory _callData
+    ) internal returns (UserOperation[] memory) {
+        bytes memory callDataForEntrypoint =
+            abi.encodeWithSignature("executeBatch(address[],uint256[],bytes[])", _target, _value, _callData);
+
+        return _setupUserOp(sender, _signerPKey, _initCode, callDataForEntrypoint);
+    }
+
     /**
      * @notice Initialize the StaticOpenfortAccount testing contract.
      * Scenario:
@@ -45,15 +129,92 @@ contract OpenfortPaymasterTest is Test {
         (accountAdmin, accountAdminPKey) = makeAddrAndKey("accountAdmin");
         vm.deal(accountAdmin, 100 ether);
 
-        // deploy entryPoint
+        // retrieve the entryPoint and deploy openfortPaymaster
         entryPoint = EntryPoint(payable(vm.envAddress("ENTRY_POINT_ADDRESS")));
         openfortPaymaster = new OpenfortPaymaster(IEntryPoint(payable(address(entryPoint))), factoryAdmin);
+
         // deploy account factory
         vm.prank(factoryAdmin);
         staticOpenfortAccountFactory = new StaticOpenfortAccountFactory(IEntryPoint(payable(address(entryPoint))));
         // deploy a new TestCounter
         testCounter = new TestCounter();
-        // deploy a new TestToken (ERC20)
+        // deploy a new TestToken (ERC20) and mint 100
         testToken = new TestToken();
+        testToken.mint(address(this), 100);
+    }
+
+    /*
+     * Test initial parameters
+     * 
+     */
+    function testInitialParameters() public {
+        assertEq(address(openfortPaymaster.entryPoint()), vm.envAddress("ENTRY_POINT_ADDRESS"));
+        assertEq(address(openfortPaymaster.owner()), factoryAdmin);
+    }
+
+    /*
+     * Test parsePaymasterAndData
+     * 
+     */
+    function testParsePaymasterData() public {
+        uint48 validUntil = 2 ** 48 - 1;
+        uint48 validAfter = 0;
+        address erc20Token = address(testToken);
+        uint256 exchangeRate = 1000;
+        bytes memory signature;
+
+        bytes32 hash = keccak256(
+                abi.encodePacked(
+                    validUntil, validAfter, erc20Token, exchangeRate));
+        { // Using scoping to avoid the "Stack too deep" error
+            (uint8 v, bytes32 r, bytes32 s) = vm.sign(factoryAdminPKey, hash);
+            signature = abi.encodePacked(r, s, v);
+        }
+        bytes memory dataEncoded = abi.encode(validUntil, validAfter, erc20Token, exchangeRate); // Looking at the source code, I've found this part was not Packed (filled with 0s)
+
+        bytes memory paymasterAndData = abi.encodePacked(address(openfortPaymaster), dataEncoded, signature); // This part was packed (filled with 0s)
+
+        uint48 returnedValidUntil;
+        uint48 returnedValidAfter;
+        address returnedVErc20Token;
+        uint256 returnedVExchangeRate;
+        bytes memory returnedSignature;
+
+        (returnedValidUntil, returnedValidAfter, returnedVErc20Token, returnedVExchangeRate, returnedSignature) = openfortPaymaster.parsePaymasterAndData(paymasterAndData);
+        assertEq(validUntil, returnedValidUntil);
+        assertEq(validAfter, returnedValidAfter);
+        assertEq(erc20Token, returnedVErc20Token);
+        assertEq(exchangeRate, returnedVExchangeRate);
+        assertEq(signature, returnedSignature);
+    }
+
+    /*
+     * The owner (factoryAdmin) can add stake
+     * Others cannot
+     */
+    function testPaymasterAddStake() public {
+        // The owner can add stake
+        vm.prank(factoryAdmin);
+        openfortPaymaster.addStake{value: 2}(1);
+
+        // Others cannot add stake
+        vm.expectRevert("Ownable: caller is not the owner");
+        openfortPaymaster.addStake{value: 2}(1);
+    }
+
+    /*
+     * Deposit 1 ETH to the EntryPoint on Paymaster's behalf
+     * 
+     */
+    function testEntryPointDepositToPaymaster() public {
+        entryPoint.depositTo{ value: 1 }(address(openfortPaymaster));
+    }
+
+    function testDepositAndReadBalance() public {
+        // Create an static account wallet and get its address
+        address account = staticOpenfortAccountFactory.createAccount(accountAdmin, "");
+        // openfortPaymaster.addDepositFor(address(testToken), account, 100);
+        // uint depositInfo = openfortPaymaster.depositInfo(address(testToken), account);
+        // assertEq(depositInfo, 100);
     }
 }
