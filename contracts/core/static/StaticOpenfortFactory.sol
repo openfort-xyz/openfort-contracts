83f3421d5df051cc96596455bc8eec1458cd749c
Eloi Manuel <eloi.manvie@gmail.com>
2023-05-18 17:49:36 +0200

    Working upgradeable and static!

--- contracts/core/static/StaticOpenfortFactory.sol
+++ contracts/core/static/StaticOpenfortFactory.sol
@@ -1,30 +1,96 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.12;
 
-// Base factory contract to inherit from
-import {BaseUpgradeableOpenfortFactory} from "../BaseUpgradeableOpenfortFactory.sol";
+import {Clones} from "@openzeppelin/contracts/proxy/Clones.sol";
 // Smart wallet implementation to use
 import {StaticOpenfortAccount} from "./StaticOpenfortAccount.sol";
+// Interfaces
+import {IBaseOpenfortFactory} from "../../interfaces/IBaseOpenfortFactory.sol";
 
 /**
  * @title StaticOpenfortFactory (Non-upgradeable)
  * @author Eloi<eloi@openfort.xyz>
- * @notice Factory to deploy StaticOpenfortAccounts
+ * @notice Contract to create account factories
  * It inherits from:
- *  - BaseOpenfortFactory because it is following the base implementation for factories
+ *  - IBaseOpenfortFactory
  */
-contract StaticOpenfortFactory is BaseUpgradeableOpenfortFactory {
-    constructor(address _entrypoint)
-        BaseUpgradeableOpenfortFactory(address(new StaticOpenfortAccount()), _entrypoint)
-    {}
+contract StaticOpenfortFactory is IBaseOpenfortFactory {
+    address public immutable entrypointContract;
+    address public immutable accountImplementation;
+
+    constructor(address _entrypoint) {
+        require(_entrypoint != address(0), "_entrypoint cannot be 0");
+        entrypointContract = _entrypoint;
+        accountImplementation = address(new StaticOpenfortAccount());
+    }
+
+    /*
+     * @notice Deploy a new Account for _admin.
+     */
+    function createAccount(address _admin, bytes calldata _data) external virtual override returns (address) {
+        address impl = address(accountImplementation);
+        bytes32 salt = keccak256(abi.encode(_admin));
+        address account = Clones.predictDeterministicAddress(impl, salt);
+
+        if (account.code.length > 0) {
+            return account;
+        }
+
+        account = Clones.cloneDeterministic(impl, salt);
+
+        _initializeAccount(account, _admin, entrypointContract, _data);
+
+        emit AccountCreated(account, _admin);
+
+        return account;
+    }
+
+    /*
+     * @notice Deploy a new Account for _admin and a given nonce.
+     */
+    function createAccountWithNonce(address _admin, bytes calldata _data, uint256 nonce)
+        external
+        virtual
+        override
+        returns (address)
+    {
+        address impl = address(accountImplementation);
+        bytes32 salt = keccak256(abi.encode(_admin, nonce));
+        address account = Clones.predictDeterministicAddress(impl, salt);
+
+        if (account.code.length > 0) {
+            return account;
+        }
+
+        account = Clones.cloneDeterministic(impl, salt);
+
+        _initializeAccount(account, _admin, entrypointContract, _data);
+
+        emit AccountCreated(account, _admin);
+
+        return account;
+    }
+
+    /*
+     * @notice Return the address of an Account that would be deployed with the given admin signer.
+     */
+    function getAddress(address _adminSigner) public view returns (address) {
+        bytes32 salt = keccak256(abi.encode(_adminSigner));
+        return Clones.predictDeterministicAddress(address(accountImplementation), salt);
+    }
+
+    /*
+     * @notice Return the address of an Account that would be deployed with the given admin signer and nonce.
+     */
+    function getAddressWithNonce(address _adminSigner, uint256 nonce) public view returns (address) {
+        bytes32 salt = keccak256(abi.encode(_adminSigner, nonce));
+        return Clones.predictDeterministicAddress(address(accountImplementation), salt);
+    }
 
     /*
      * @dev Called in `createAccount`. Initializes the account contract created in `createAccount`.
      */
-    function _initializeAccount(address _account, address _admin, address _entrypointContract, bytes calldata _data)
-        internal
-        override
-    {
+    function _initializeAccount(address _account, address _admin, address _entrypointContract, bytes calldata _data) internal {
         StaticOpenfortAccount(payable(_account)).initialize(_admin, _entrypointContract, _data);
     }
 }
--- contracts/core/upgradeable/UpgradeableOpenfortFactory.sol
+++ contracts/core/upgradeable/UpgradeableOpenfortFactory.sol
@@ -1,28 +1,97 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.12;
 
-// Base factory contract to inherit from
-import {BaseUpgradeableOpenfortFactory} from "../BaseUpgradeableOpenfortFactory.sol";
+import "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+import "@openzeppelin/contracts/utils/Create2.sol";
 // Smart wallet implementation to use
 import {UpgradeableOpenfortAccount} from "./UpgradeableOpenfortAccount.sol";
+// Interfaces
+import {IBaseOpenfortFactory} from "../../interfaces/IBaseOpenfortFactory.sol";
 
 /**
  * @title UpgradeableOpenfortFactory (Non-upgradeable)
  * @author Eloi<eloi@openfort.xyz>
  * @notice Factory to deploy UpgradeableOpenfortAccounts
  * It inherits from:
- *  - BaseUpgradeableOpenfortFactory because it is following the base implementation for factories
+ *  - IBaseOpenfortFactory
  */
-contract UpgradeableOpenfortFactory is BaseUpgradeableOpenfortFactory {
-    constructor(address _entrypoint) BaseUpgradeableOpenfortFactory(address(new UpgradeableOpenfortAccount()), _entrypoint) {}
+contract UpgradeableOpenfortFactory is IBaseOpenfortFactory {
+    address public immutable entrypointContract;
+    address public immutable accountImplementation;
+
+    constructor(address _entrypoint) {
+        require(_entrypoint != address(0), "_entrypoint cannot be 0");
+        entrypointContract = _entrypoint;
+        accountImplementation = address(new UpgradeableOpenfortAccount());
+    }
 
     /*
-     * @dev Called in `createAccount`. Initializes the account contract created in `createAccount`.
+     * @notice Deploy a new Account for _admin.
      */
-    function _initializeAccount(address _account, address _admin, address _entrypointContract, bytes calldata _data)
-        internal
-        override
-    {
-        UpgradeableOpenfortAccount(payable(_account)).initialize(_admin, _entrypointContract, _data);
+    function createAccount(address _admin, bytes calldata _data) external returns (address) {
+        bytes32 salt = keccak256(abi.encode(_admin));
+        address account = getAddress(_admin);
+        
+        if (account.code.length > 0) {
+            return account;
+        }
+
+        UpgradeableOpenfortAccount newUpgradeableOpenfortAccount = UpgradeableOpenfortAccount(payable(new ERC1967Proxy{salt : bytes32(salt)}(
+                address(accountImplementation),
+                abi.encodeCall(UpgradeableOpenfortAccount.initialize, (_admin, entrypointContract, _data))
+            )));
+
+        emit AccountCreated(account, _admin);
+
+        return address(newUpgradeableOpenfortAccount);
+    }
+
+    /*
+     * @notice Deploy a new Account for _admin.
+     */
+    function createAccountWithNonce(address _admin, bytes calldata _data,uint256 nonce) external returns (address) {
+        bytes32 salt = keccak256(abi.encode(_admin, nonce));
+        address account = getAddressWithNonce(_admin, nonce);
+        
+        if (account.code.length > 0) {
+            return account;
+        }
+
+        UpgradeableOpenfortAccount newUpgradeableOpenfortAccount = UpgradeableOpenfortAccount(payable(new ERC1967Proxy{salt : bytes32(salt)}(
+                address(accountImplementation),
+                abi.encodeCall(UpgradeableOpenfortAccount.initialize, (_admin, entrypointContract, _data))
+            )));
+
+        emit AccountCreated(account, _admin);
+
+        return address(newUpgradeableOpenfortAccount);
+    }
+
+    /*
+     * @notice Return the address of an Account that would be deployed with the given admin signer.
+     */
+    function getAddress(address _admin) public view returns (address) {
+        bytes32 salt = keccak256(abi.encode(_admin));
+        return Create2.computeAddress(bytes32(salt), keccak256(abi.encodePacked(
+                type(ERC1967Proxy).creationCode,
+                abi.encode(
+                    address(accountImplementation),
+                    abi.encodeCall(UpgradeableOpenfortAccount.initialize, (_admin, entrypointContract, ""))
+                )
+            )));
+    }
+
+    /*
+     * @notice Return the address of an Account that would be deployed with the given admin signer.
+     */
+    function getAddressWithNonce(address _admin, uint256 nonce) public view returns (address) {
+        bytes32 salt = keccak256(abi.encode(_admin, nonce));
+        return Create2.computeAddress(bytes32(salt), keccak256(abi.encodePacked(
+                type(ERC1967Proxy).creationCode,
+                abi.encode(
+                    address(accountImplementation),
+                    abi.encodeCall(UpgradeableOpenfortAccount.initialize, (_admin, entrypointContract, ""))
+                )
+            )));
     }
 }
