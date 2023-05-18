358853a1dfe963d8940eb6fa3289a61250a8a94c
Eloi Manuel <eloi.manvie@gmail.com>
2023-05-18 15:33:50 +0200

    Deployment improvements

---
+++ script/deployStaticAccounts.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.12;
+
+import {Script} from "forge-std/Script.sol";
+import {StaticOpenfortFactory} from "../contracts/core/static/StaticOpenfortFactory.sol";
+import {TestCounter} from "account-abstraction/test/TestCounter.sol";
+
+contract StaticOpenfortDeploy is Script {
+    uint256 deployPrivKey;
+    address deployAddress;
+
+    function setUp() public {
+        deployPrivKey = vm.deriveKey(vm.envString("MNEMONIC"), 0);
+        deployAddress = vm.addr(deployPrivKey);
+    }
+
+    function run() public {
+        vm.startBroadcast(deployPrivKey);
+
+        StaticOpenfortFactory staticOpenfortFactory = new StaticOpenfortFactory((payable(vm.envAddress("ENTRY_POINT_ADDRESS"))));
+        staticOpenfortFactory.accountImplementation();
+  
+        // The first call should create a new account, while the second will just return the corresponding account address
+        staticOpenfortFactory.createAccount(deployAddress, bytes(""));
+        staticOpenfortFactory.createAccount(deployAddress, bytes(""));
+
+        vm.stopBroadcast();
+    }
+}
