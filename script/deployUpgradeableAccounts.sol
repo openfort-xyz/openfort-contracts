f1aca438ff1e2e8d216ad4e691f601ac2ba105b4
Eloi Manuel <eloi.manvie@gmail.com>
2023-05-19 10:55:25 +0200

    Script to test UpgradeableOpenfort deployments

--- script/deployUpgradeableAccounts.sol
+++ script/deployUpgradeableAccounts.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.12;
 
 import {Script} from "forge-std/Script.sol";
-import {UpgradeableOpenfortFactory} from "../contracts/core/upgradeable/UpgradeableOpenfortFactory.sol";
+import {UpgradeableOpenfortFactory, UpgradeableOpenfortAccount} from "../contracts/core/upgradeable/UpgradeableOpenfortFactory.sol";
 import {TestCounter} from "account-abstraction/test/TestCounter.sol";
 
 contract UpgradeableOpenfortDeploy is Script {
@@ -18,11 +18,18 @@ contract UpgradeableOpenfortDeploy is Script {
         vm.startBroadcast(deployPrivKey);
 
         UpgradeableOpenfortFactory upgradeableOpenfortFactory = new UpgradeableOpenfortFactory((payable(vm.envAddress("ENTRY_POINT_ADDRESS"))));
-        upgradeableOpenfortFactory.accountImplementation();
+        address account1 = upgradeableOpenfortFactory.accountImplementation();
   
         // The first call should create a new account, while the second will just return the corresponding account address
-        upgradeableOpenfortFactory.createAccount(deployAddress, bytes(""));
-        upgradeableOpenfortFactory.createAccount(deployAddress, bytes(""));
+        address account2 = upgradeableOpenfortFactory.createAccount(deployAddress, bytes(""));
+        address account3 = upgradeableOpenfortFactory.createAccount(deployAddress, bytes(""));
+
+        assert(account1 != account2);
+        assert(account2 == account3);
+
+        UpgradeableOpenfortAccount newAccount = new UpgradeableOpenfortAccount();
+
+        UpgradeableOpenfortAccount(payable(account2)).upgradeTo(address(newAccount));
 
         vm.stopBroadcast();
     }
