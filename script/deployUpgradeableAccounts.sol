4bafab0a628efe342d6b3fee09efdc54828f0ac7
Eloi Manuel <eloi.manvie@gmail.com>
2023-05-22 14:11:00 +0200

    Improved scripting

--- script/deployUpgradeableAccounts.sol
+++ script/deployUpgradeableAccounts.sol
@@ -1,31 +1,28 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.12;
 
-import {Script} from "forge-std/Script.sol";
-import {
-    UpgradeableOpenfortFactory,
-    UpgradeableOpenfortAccount
-} from "../contracts/core/upgradeable/UpgradeableOpenfortFactory.sol";
-import {TestCounter} from "account-abstraction/test/TestCounter.sol";
+import {Script, console} from "forge-std/Script.sol";
+import {UpgradeableOpenfortFactory} from "../contracts/core/upgradeable/UpgradeableOpenfortFactory.sol";
+import {IEntryPoint} from "lib/account-abstraction/contracts/interfaces/IEntryPoint.sol";
 
 contract UpgradeableOpenfortDeploy is Script {
-    uint256 deployPrivKey;
-    address deployAddress;
+    uint256 internal deployPrivKey = vm.deriveKey(vm.envString("MNEMONIC"), 0);
+    address internal deployAddress = makeAddr(vm.envString("MNEMONIC"));
+    IEntryPoint internal entryPoint = IEntryPoint((payable(vm.envAddress("ENTRY_POINT_ADDRESS"))));
 
-    function setUp() public {
-        deployPrivKey = vm.deriveKey(vm.envString("MNEMONIC"), 0);
-        deployAddress = vm.addr(deployPrivKey);
-    }
+    function setUp() public {}
 
     function run() public {
         vm.startBroadcast(deployPrivKey);
 
-        UpgradeableOpenfortFactory upgradeableOpenfortFactory =
-            new UpgradeableOpenfortFactory((payable(vm.envAddress("ENTRY_POINT_ADDRESS"))));
+        UpgradeableOpenfortFactory upgradeableOpenfortFactory = new UpgradeableOpenfortFactory(address(entryPoint));
         //address account1 = upgradeableOpenfortFactory.accountImplementation();
 
         // The first call should create a new account, while the second will just return the corresponding account address
         address account2 = upgradeableOpenfortFactory.createAccount(deployAddress, bytes(""));
+        console.log(
+            "Factory at address %s has created an account at address %s", address(upgradeableOpenfortFactory), account2
+        );
         //address account3 = upgradeableOpenfortFactory.createAccount(deployAddress, bytes(""));
 
         //assert(account1 != account2);
