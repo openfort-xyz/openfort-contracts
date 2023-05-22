d3a18edc7b46a340f794158e2be565d3ea053f14
Eloi Manuel <eloi.manvie@gmail.com>
2023-05-22 14:11:00 +0200

    Improved scripting

--- script/deployStaticAccounts.sol
+++ script/deployStaticAccounts.sol
@@ -1,28 +1,28 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.12;
 
-import {Script} from "forge-std/Script.sol";
+import {Script, console} from "forge-std/Script.sol";
+import {IEntryPoint} from "lib/account-abstraction/contracts/interfaces/IEntryPoint.sol";
 import {StaticOpenfortFactory} from "../contracts/core/static/StaticOpenfortFactory.sol";
-import {TestCounter} from "account-abstraction/test/TestCounter.sol";
 
 contract StaticOpenfortDeploy is Script {
-    uint256 deployPrivKey;
-    address deployAddress;
+    uint256 internal deployPrivKey = vm.deriveKey(vm.envString("MNEMONIC"), 0);
+    address internal deployAddress = makeAddr(vm.envString("MNEMONIC"));
+    IEntryPoint internal entryPoint = IEntryPoint((payable(vm.envAddress("ENTRY_POINT_ADDRESS"))));
 
-    function setUp() public {
-        deployPrivKey = vm.deriveKey(vm.envString("MNEMONIC"), 0);
-        deployAddress = vm.addr(deployPrivKey);
-    }
+    function setUp() public {}
 
     function run() public {
         vm.startBroadcast(deployPrivKey);
 
-        StaticOpenfortFactory staticOpenfortFactory =
-            new StaticOpenfortFactory((payable(vm.envAddress("ENTRY_POINT_ADDRESS"))));
+        StaticOpenfortFactory staticOpenfortFactory = new StaticOpenfortFactory(address(entryPoint));
         staticOpenfortFactory.accountImplementation();
 
         // The first call should create a new account, while the second will just return the corresponding account address
-        staticOpenfortFactory.createAccount(deployAddress, bytes(""));
+        address account2 = staticOpenfortFactory.createAccount(deployAddress, bytes(""));
+        console.log(
+            "Factory at address %s has created an account at address %s", address(staticOpenfortFactory), account2
+        );
         // staticOpenfortFactory.createAccount(deployAddress, bytes(""));
 
         vm.stopBroadcast();
