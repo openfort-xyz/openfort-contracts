39318911db1fcbc4647f2d1a908cbb094047e7cc
Eloi Manuel <eloi.manvie@gmail.com>
2023-05-24 14:38:40 +0200

    Solidity upgrade to 0.8.19

--- script/Utils/GasCalculations.sol
+++ script/Utils/GasCalculations.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.17;
+pragma solidity ^0.8.17;
 
 import {UserOperation, UserOperationLib} from "lib/account-abstraction/contracts/interfaces/UserOperation.sol";
 
