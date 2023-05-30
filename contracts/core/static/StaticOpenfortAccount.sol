e711f54d56ad6df0a0ca644b980f0dcd9a94d013
Eloi Manuel <eloi.manvie@gmail.com>
2023-05-30 11:36:03 +0200

    Small style updates

--- contracts/core/static/StaticOpenfortAccount.sol
+++ contracts/core/static/StaticOpenfortAccount.sol
@@ -8,18 +8,18 @@ import {BaseOpenfortAccount} from "../BaseOpenfortAccount.sol";
  * @title StaticOpenfortAccount (Non-upgradeable)
  * @author Eloi<eloi@openfort.xyz>
  * @notice Minimal smart contract wallet with session keys following the ERC-4337 standard.
+ * The EntryPoint can be updated via updateEntryPoint()
  * It inherits from:
- *  - BaseUpgradeableOpenfortAccount
- *  - The EntryPoint can be updated via updateEntryPoint()
+ *  - BaseOpenfortAccount
  */
 contract StaticOpenfortAccount is BaseOpenfortAccount {
     event EntryPointUpdated(address oldEntryPoint, address newEntryPoint);
 
     /*
-     * @notice Initializes the smart contract wallet.
+     * @notice Initialize the smart contract wallet.
      */
     function initialize(address _defaultAdmin, address _entrypoint, bytes calldata) public override initializer {
-        if(_defaultAdmin == address(0) || _entrypoint == address(0)) {
+        if (_defaultAdmin == address(0) || _entrypoint == address(0)) {
             revert ZeroAddressNotAllowed();
         }
         emit EntryPointUpdated(entrypointContract, _entrypoint);
@@ -31,7 +31,7 @@ contract StaticOpenfortAccount is BaseOpenfortAccount {
      * Update the EntryPoint address
      */
     function updateEntryPoint(address _newEntrypoint) external onlyOwner {
-        if(_newEntrypoint == address(0)) {
+        if (_newEntrypoint == address(0)) {
             revert ZeroAddressNotAllowed();
         }
         emit EntryPointUpdated(entrypointContract, _newEntrypoint);
--- contracts/core/upgradeable/UpgradeableOpenfortFactory.sol
+++ contracts/core/upgradeable/UpgradeableOpenfortFactory.sol
@@ -20,7 +20,7 @@ contract UpgradeableOpenfortFactory is IBaseOpenfortFactory {
     address public immutable accountImplementation;
 
     constructor(address _entrypoint) {
-        if(_entrypoint == address(0)) {
+        if (_entrypoint == address(0)) {
             revert ZeroAddressNotAllowed();
         }
         entrypointContract = _entrypoint;
@@ -52,7 +52,7 @@ contract UpgradeableOpenfortFactory is IBaseOpenfortFactory {
     }
 
     /*
-     * @notice Deploy a new Account for _admin.
+     * @notice Deploy a new account for _admin with a nonce.
      */
     function createAccountWithNonce(address _admin, bytes calldata _data, uint256 nonce)
         external
@@ -79,7 +79,7 @@ contract UpgradeableOpenfortFactory is IBaseOpenfortFactory {
     }
 
     /*
-     * @notice Return the address of an Account that would be deployed with the given admin signer.
+     * @notice Return the address of an account that would be deployed with the given admin signer.
      */
     function getAddress(address _admin) public view returns (address) {
         bytes32 salt = keccak256(abi.encode(_admin));
@@ -98,7 +98,7 @@ contract UpgradeableOpenfortFactory is IBaseOpenfortFactory {
     }
 
     /*
-     * @notice Return the address of an Account that would be deployed with the given admin signer.
+     * @notice Return the address of an account that would be deployed with the given admin signer and nonce.
      */
     function getAddressWithNonce(address _admin, uint256 nonce) public view returns (address) {
         bytes32 salt = keccak256(abi.encode(_admin, nonce));
