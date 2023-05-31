9e3c332926e2b5e6041cf687a5ccdd9a484798e0
Eloi Manuel <eloi.manvie@gmail.com>
2023-05-31 12:56:44 +0200

    updated cloc script to ignore mock contracts

--- script/count-slocs.sh
+++ script/count-slocs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Counting all lines of Solidity files: "
-find contracts -name '*.sol' | xargs wc -l
+find contracts -name '*.sol' ! -path "*/mock/*" | xargs wc -l
 echo
 echo "Counting all lines of Solidity files (excluding interfaces): "
-find contracts -name '*.sol' ! -path "*/interfaces/*"  | xargs wc -l
+find contracts -name '*.sol' ! -path "*/mock/*" ! -path "*/interfaces/*"  | xargs wc -l
