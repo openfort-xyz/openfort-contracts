71a7b0e7ebc6d6eb409ba3d5fa804307d75cc5c5
Eloi Manuel <eloi.manvie@gmail.com>
2023-05-31 16:47:47 +0200

    Unifying licenses

--- contracts/core/managed/ManagedOpenfortFactory.sol
+++ contracts/core/managed/ManagedOpenfortFactory.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.12;
 
 import {BeaconProxy} from "@openzeppelin/contracts/proxy/beacon/BeaconProxy.sol";
