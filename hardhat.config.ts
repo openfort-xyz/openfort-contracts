fc90e6652a38b4b57fbba5e363f224c7042730a8
Eloi Manuel <eloi.manvie@gmail.com>
2023-06-16 15:05:08 +0200

    Updating versions

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -17,7 +17,7 @@ if (!mnemonic) {
 const config: HardhatUserConfig = {
   defaultNetwork: "hardhat",
   solidity: {
-    version: "0.8.17",
+    version: "0.8.19",
     settings: {
       optimizer: { enabled: true },
     },
