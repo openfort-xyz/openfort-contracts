9692a8bfaec09ca60e9cf04c2e49ef7710f90c05
Eloi Manuel <eloi.manvie@gmail.com>
2023-06-19 19:09:09 +0200

    Added support for Arbitrum Goerli

--- script/deployAllChains.sh
+++ script/deployAllChains.sh
@@ -4,28 +4,34 @@ source .env
 LOG_FILE=$(date +%Y-%m-%d_%H:%M)"-deploymentAllChains.log"
 
 echo "------ StaticOpenfortDeploy ------ (Goerli)"
-forge script StaticOpenfortDeploy --fork-url $GOERLI_RPC -vvvv --verify --broadcast --etherscan-api-key $GOERLI_API_KEY >> $LOG_FILE
+forge script StaticOpenfortDeploy --rpc-url $GOERLI_RPC -vvvv --verify --broadcast --etherscan-api-key $GOERLI_API_KEY >> $LOG_FILE
 echo "------ StaticOpenfortDeploy ------ (Mumbai)"
-forge script StaticOpenfortDeploy --fork-url $POLYGON_MUMBAI_RPC -vvvv --verify --broadcast --etherscan-api-key $POLYGON_MUMBAI_KEY >> $LOG_FILE
+forge script StaticOpenfortDeploy --rpc-url $POLYGON_MUMBAI_RPC -vvvv --verify --broadcast --etherscan-api-key $POLYGON_MUMBAI_KEY >> $LOG_FILE
 echo "------ StaticOpenfortDeploy ------ (Fuji)"
-forge script StaticOpenfortDeploy --fork-url $AVALANCHE_FUJI_RPC -vvvv --verify --broadcast --etherscan-api-key $FUJI_API_KEY >> $LOG_FILE
+forge script StaticOpenfortDeploy --rpc-url $AVALANCHE_FUJI_RPC -vvvv --verify --broadcast --etherscan-api-key $FUJI_API_KEY >> $LOG_FILE
 echo "------ StaticOpenfortDeploy ------ (BSC testnet)"
-forge script StaticOpenfortDeploy --fork-url $BSC_TESTNET_RPC -vvvv --verify --broadcast --etherscan-api-key $BSCSCAN_TESTNET_API_KEY >> $LOG_FILE
+forge script StaticOpenfortDeploy --rpc-url $BSC_TESTNET_RPC -vvvv --verify --broadcast --etherscan-api-key $BSCSCAN_TESTNET_API_KEY >> $LOG_FILE
+echo "------ StaticOpenfortDeploy ------ (Arbitrum Goerli testnet)"
+forge script StaticOpenfortDeploy --rpc-url $ARBITRUM_GOERLI_RPC -vvvv --verify --broadcast --etherscan-api-key $ARBISCAN_API_KEY >> $LOG_FILE
 
 echo "------ UpgradeableOpenfortDeploy ------ (Goerli)"
-forge script UpgradeableOpenfortDeploy --fork-url $GOERLI_RPC -vvvv --verify --broadcast --etherscan-api-key $GOERLI_API_KEY >> $LOG_FILE
+forge script UpgradeableOpenfortDeploy --rpc-url $GOERLI_RPC -vvvv --verify --broadcast --etherscan-api-key $GOERLI_API_KEY >> $LOG_FILE
 echo "------ UpgradeableOpenfortDeploy ------ (Mumbai)"
-forge script UpgradeableOpenfortDeploy --fork-url $POLYGON_MUMBAI_RPC -vvvv --verify --broadcast --etherscan-api-key $POLYGON_MUMBAI_KEY >> $LOG_FILE
+forge script UpgradeableOpenfortDeploy --rpc-url $POLYGON_MUMBAI_RPC -vvvv --verify --broadcast --legacy --etherscan-api-key $POLYGON_MUMBAI_KEY >> $LOG_FILE
 echo "------ UpgradeableOpenfortDeploy ------ (Fuji)"
-forge script UpgradeableOpenfortDeploy --fork-url $AVALANCHE_FUJI_RPC -vvvv --verify --broadcast --etherscan-api-key $FUJI_API_KEY >> $LOG_FILE
+forge script UpgradeableOpenfortDeploy --rpc-url $AVALANCHE_FUJI_RPC -vvvv --verify --broadcast --etherscan-api-key $FUJI_API_KEY >> $LOG_FILE
 echo "------ UpgradeableOpenfortDeploy ------ (BSC testnet)"
-forge script UpgradeableOpenfortDeploy --fork-url $BSC_TESTNET_RPC -vvvv --verify --broadcast --etherscan-api-key $BSCSCAN_TESTNET_API_KEY >> $LOG_FILE
+forge script UpgradeableOpenfortDeploy --rpc-url $BSC_TESTNET_RPC -vvvv --verify --broadcast --etherscan-api-key $BSCSCAN_TESTNET_API_KEY >> $LOG_FILE
+echo "------ UpgradeableOpenfortDeploy ------ (Arbitrum Goerli testnet)"
+forge script UpgradeableOpenfortDeploy --rpc-url $ARBITRUM_GOERLI_RPC -vvvv --verify --broadcast --etherscan-api-key $ARBISCAN_API_KEY >> $LOG_FILE
 
 echo "------ ManagedOpenfortDeploy ------ (Goerli)"
-forge script ManagedOpenfortDeploy --fork-url $GOERLI_RPC -vvvv --verify --broadcast --etherscan-api-key $GOERLI_API_KEY >> $LOG_FILE
+forge script ManagedOpenfortDeploy --rpc-url $GOERLI_RPC -vvvv --verify --broadcast --etherscan-api-key $GOERLI_API_KEY >> $LOG_FILE
 echo "------ ManagedOpenfortDeploy ------ (Mumbai)"
-forge script ManagedOpenfortDeploy --fork-url $POLYGON_MUMBAI_RPC -vvvv --verify --broadcast --etherscan-api-key $POLYGON_MUMBAI_KEY >> $LOG_FILE
+forge script ManagedOpenfortDeploy --rpc-url $POLYGON_MUMBAI_RPC -vvvv --verify --broadcast --etherscan-api-key $POLYGON_MUMBAI_KEY >> $LOG_FILE
 echo "------ ManagedOpenfortDeploy ------ (Fuji)"
-forge script ManagedOpenfortDeploy --fork-url $AVALANCHE_FUJI_RPC -vvvv --verify --broadcast --etherscan-api-key $FUJI_API_KEY >> $LOG_FILE
+forge script ManagedOpenfortDeploy --rpc-url $AVALANCHE_FUJI_RPC -vvvv --verify --broadcast --etherscan-api-key $FUJI_API_KEY >> $LOG_FILE
 echo "------ ManagedOpenfortDeploy ------ (BSC testnet)"
-forge script ManagedOpenfortDeploy --fork-url $BSC_TESTNET_RPC -vvvv --verify --broadcast --etherscan-api-key $BSCSCAN_TESTNET_API_KEY >> $LOG_FILE
+forge script ManagedOpenfortDeploy --rpc-url $BSC_TESTNET_RPC -vvvv --verify --broadcast --etherscan-api-key $BSCSCAN_TESTNET_API_KEY >> $LOG_FILE
+echo "------ ManagedOpenfortDeploy ------ (Arbitrum Goerli testnet)"
+forge script ManagedOpenfortDeploy --rpc-url $ARBITRUM_GOERLI_RPC -vvvv --verify --broadcast --etherscan-api-key $ARBISCAN_API_KEY >> $LOG_FILE
