5c5a3b5cdfe2ee498e3f9664276f5d99eea09f43
Eloi Manuel <eloi.manvie@gmail.com>
2023-07-11 18:36:45 +0200

    latest deploy script mods

--- script/deployMock.sol
+++ script/deployMock.sol
@@ -9,6 +9,7 @@ import {Rewards} from "../contracts/mock/Rewards.sol";
 
 contract DeployMock is Script {
     uint256 internal deployPrivKey = vm.deriveKey(vm.envString("MNEMONIC"), 0);
+    // uint256 internal deployPrivKey = vm.envUint("PK");
     address internal deployAddress = vm.addr(deployPrivKey);
     IEntryPoint internal entryPoint = IEntryPoint((payable(vm.envAddress("ENTRY_POINT_ADDRESS"))));
 
