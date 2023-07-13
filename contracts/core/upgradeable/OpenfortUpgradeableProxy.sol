89d016def48ed95ef83764e0ea0cbbc92c1c5254
Eloi Manuel <eloi.manvie@gmail.com>
2023-07-13 18:28:17 +0200

    Adding implementation() function to proxies

--- contracts/core/upgradeable/OpenfortUpgradeableProxy.sol
+++ contracts/core/upgradeable/OpenfortUpgradeableProxy.sol
@@ -12,4 +12,8 @@ import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.s
  */
 contract OpenfortUpgradeableProxy is ERC1967Proxy {
     constructor(address _logic, bytes memory _data) ERC1967Proxy(_logic, _data) {}
+
+    function implementation() external view returns (address) {
+        return _getImplementation();
+    }
 }
