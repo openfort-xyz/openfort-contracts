0177f9824011497c722e31be6ec73908f80b1b73
Eloi Manuel <eloi.manvie@gmail.com>
2023-07-13 18:28:17 +0200

    Adding implementation() function to proxies

--- script/deployAllChains.sh
+++ script/deployAllChains.sh
@@ -19,21 +19,21 @@ LOG_FILE=script/deployments/$(date +%Y-%m-%d_%H:%M)"-deploymentAllChains.log"
 # forge script StaticOpenfortDeploy --rpc-url $ARBITRUM_GOERLI_RPC -vvvv --verify --broadcast --slow --etherscan-api-key $ARBISCAN_API_KEY >> $LOG_FILE
 # sleep 3
 
-echo "------ UpgradeableOpenfortDeploy ------ (Goerli)"
-forge script UpgradeableOpenfortDeploy --rpc-url $GOERLI_RPC -vvvv --verify --broadcast --slow --etherscan-api-key $GOERLI_API_KEY >> $LOG_FILE
-sleep 3
-echo "------ UpgradeableOpenfortDeploy ------ (Mumbai)"
-forge script UpgradeableOpenfortDeploy --rpc-url $POLYGON_MUMBAI_RPC -vvvv --verify --broadcast --slow --legacy --etherscan-api-key $POLYGON_MUMBAI_KEY >> $LOG_FILE
-sleep 3
+# echo "------ UpgradeableOpenfortDeploy ------ (Goerli)"
+# forge script UpgradeableOpenfortDeploy --rpc-url $GOERLI_RPC -vvvv --verify --broadcast --slow -g 150 --etherscan-api-key $GOERLI_API_KEY >> $LOG_FILE
+# sleep 3
+# echo "------ UpgradeableOpenfortDeploy ------ (Mumbai)"
+# forge script UpgradeableOpenfortDeploy --rpc-url $POLYGON_MUMBAI_RPC -vvvv --verify --broadcast --slow --legacy --etherscan-api-key $POLYGON_MUMBAI_KEY >> $LOG_FILE
+# sleep 3
 echo "------ UpgradeableOpenfortDeploy ------ (Fuji)"
 forge script UpgradeableOpenfortDeploy --rpc-url $AVALANCHE_FUJI_RPC -vvvv --verify --broadcast --slow --etherscan-api-key $FUJI_API_KEY >> $LOG_FILE
 sleep 3
-echo "------ UpgradeableOpenfortDeploy ------ (BSC testnet)"
-forge script UpgradeableOpenfortDeploy --rpc-url $BSC_TESTNET_RPC -vvvv --verify --broadcast --slow --etherscan-api-key $BSCSCAN_TESTNET_API_KEY >> $LOG_FILE
-sleep 3
-echo "------ UpgradeableOpenfortDeploy ------ (Arbitrum Goerli testnet)"
-forge script UpgradeableOpenfortDeploy --rpc-url $ARBITRUM_GOERLI_RPC -vvvv --verify --broadcast --slow -g 200 --etherscan-api-key $ARBISCAN_API_KEY >> $LOG_FILE
-sleep 3
+# echo "------ UpgradeableOpenfortDeploy ------ (BSC testnet)"
+# forge script UpgradeableOpenfortDeploy --rpc-url $BSC_TESTNET_RPC -vvvv --verify --broadcast --slow --etherscan-api-key $BSCSCAN_TESTNET_API_KEY >> $LOG_FILE
+# sleep 3
+# echo "------ UpgradeableOpenfortDeploy ------ (Arbitrum Goerli testnet)"
+# forge script UpgradeableOpenfortDeploy --rpc-url $ARBITRUM_GOERLI_RPC -vvvv --verify --broadcast --slow -g 200 --etherscan-api-key $ARBISCAN_API_KEY >> $LOG_FILE
+# sleep 3
 
 # echo "------ ManagedOpenfortDeploy ------ (Goerli)"
 # forge script ManagedOpenfortDeploy --rpc-url $GOERLI_RPC -vvvv --verify --broadcast --slow --etherscan-api-key $GOERLI_API_KEY >> $LOG_FILE
