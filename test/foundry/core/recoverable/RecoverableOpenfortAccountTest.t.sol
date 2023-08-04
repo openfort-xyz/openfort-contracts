1ec5322d872ecc5033cbe60e9696abc383b9658d
Eloi Manuel <eloi.manvie@gmail.com>
2023-08-04 16:38:09 +0200

    Fixing RECOVER_TYPEHASH

--- test/foundry/core/recoverable/RecoverableOpenfortAccountTest.t.sol
+++ test/foundry/core/recoverable/RecoverableOpenfortAccountTest.t.sol
@@ -61,8 +61,8 @@ contract RecoverableOpenfortAccountTest is Test {
     error InvalidRecoverySignatures();
     error InvalidSignatureAmount();
 
-    // keccak256("Recover(address recoveryAddress,uint64 executeAfter,uint32 guardiansRequired"));
-    bytes32 RECOVER_TYPEHASH = 0x601b3fa0ae18d2a4c446b40cd6b8e0e911bbbb5dd66b248922e3d91efafa0969;
+    // keccak256("Recover(address recoveryAddress,uint64 executeAfter,uint32 guardiansRequired)");
+    bytes32 RECOVER_TYPEHASH = 0x9f7aca777caf11405930359f601a4db01fad1b2d79ef3f2f9e93c835e9feffa5;
 
     /*
      * Auxiliary function to generate a userOP
