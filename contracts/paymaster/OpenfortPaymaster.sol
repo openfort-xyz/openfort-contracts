d8b643356b60f41c1e23745485573a3d3c5030d8
Eloi Manuel <eloi.manvie@gmail.com>
2023-09-18 17:51:05 +0200

    Adding tokenRecipient and fixed ERC20 payments

--- contracts/paymaster/OpenfortPaymaster.sol
+++ contracts/paymaster/OpenfortPaymaster.sol
@@ -5,17 +5,12 @@ import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
-import {BaseAccount, UserOperation, UserOperationLib, IEntryPoint} from "account-abstraction/core/BaseAccount.sol";
+import {UserOperation, UserOperationLib, IEntryPoint} from "account-abstraction/core/BaseAccount.sol";
 import {BasePaymaster} from "account-abstraction/core/BasePaymaster.sol";
 import "account-abstraction/core/Helpers.sol" as Helpers;
 
 /**
- * A paymaster based on the eth-infinitism sample VerifyingPaymaster contract and Stackups.
- * It has the same functionality as the sample, but with added support for withdrawing ERC20 tokens.
- * All withdrawn tokens will be transferred to a designated address.
- * Note that the off-chain signer should have a strategy in place to handle a failed token withdrawal.
- *
- * See account-abstraction/contracts/samples/VerifyingPaymaster.sol for detailed comments.
+ * A paymaster inspired in the VerifyingPaymaster samples from eth-infinitism and Stackups' modification.
  */
 contract OpenfortPaymaster is BasePaymaster {
     using ECDSA for bytes32;
@@ -23,13 +18,31 @@ contract OpenfortPaymaster is BasePaymaster {
     using SafeERC20 for IERC20;
 
     uint256 private constant VALID_PND_OFFSET = 20; // length of an address
-    uint256 private constant SIGNATURE_OFFSET = 148; // 48+48+20+32 = 148
+    uint256 private constant SIGNATURE_OFFSET = 180; // 20+48+48+64 = 180
     uint256 private constant POST_OP_GAS = 35000;
 
+    address public tokenRecipient;
+
+    enum Mode {
+        PayForUser,
+        DynamicRate,
+        FixedRate
+    }
+
+    struct PolicyStrategy {
+        Mode paymasterMode;
+        address erc20Token;
+        uint256 exchangeRate;
+    }
+
+    error InvalidTokenRecipient();
+
     event GasPaidInERC20(address ERC20, uint256 actualGasCost, uint256 actualTokensSent);
+    event TokenRecipientUpdated(address oldTokenRecipient, address newTokenRecipient);
 
     constructor(IEntryPoint _entryPoint, address _owner) BasePaymaster(_entryPoint) {
         _transferOwnership(_owner);
+        tokenRecipient = _owner;
     }
 
     /**
@@ -43,8 +56,7 @@ contract OpenfortPaymaster is BasePaymaster {
         UserOperation calldata userOp,
         uint48 validUntil,
         uint48 validAfter,
-        address erc20Token,
-        uint256 exchangeRate
+        PolicyStrategy memory strategy
     ) public view returns (bytes32) {
         // Dividing the hashing in 2 parts due to the stack too deep error
         bytes memory firstHalf = abi.encode(
@@ -58,16 +70,23 @@ contract OpenfortPaymaster is BasePaymaster {
             userOp.maxFeePerGas,
             userOp.maxPriorityFeePerGas
         );
-        bytes memory secondHalf =
-            abi.encode(block.chainid, address(this), validUntil, validAfter, erc20Token, exchangeRate);
+        bytes memory secondHalf = abi.encode(
+            block.chainid,
+            address(this),
+            validUntil,
+            validAfter,
+            strategy.paymasterMode,
+            strategy.erc20Token,
+            strategy.exchangeRate
+        );
         return keccak256(abi.encodePacked(firstHalf, secondHalf));
     }
 
     /**
-     * Verify our external signer signed this request.
+     * Verify paymaster owner signed this request.
      * The "paymasterAndData" is expected to be the paymaster and a signature over the entire request params
      * paymasterAndData[:20]: address(this)
-     * paymasterAndData[20:148]: abi.encode(validUntil, validAfter, erc20Token, exchangeRate) // 48+48+20+32
+     * paymasterAndData[20:148]: abi.encode(validUntil, validAfter, strategy) // 20+48+48+64
      * paymasterAndData[SIGNATURE_OFFSET:]: signature
      */
     function _validatePaymasterUserOp(
@@ -75,77 +94,87 @@ contract OpenfortPaymaster is BasePaymaster {
         bytes32, /*userOpHash*/
         uint256 /*requiredPreFund*/
     ) internal view override returns (bytes memory context, uint256 validationData) {
-        (uint48 validUntil, uint48 validAfter, address erc20Token, uint256 exchangeRate, bytes calldata signature) =
+        (uint48 validUntil, uint48 validAfter, PolicyStrategy memory strategy, bytes calldata signature) =
             parsePaymasterAndData(userOp.paymasterAndData);
-        // solhint-disable-next-line reason-string
-        require(
-            signature.length == 64 || signature.length == 65,
-            "VerifyingPaymaster: invalid signature length in paymasterAndData"
-        );
-        bytes32 hash = ECDSA.toEthSignedMessageHash(getHash(userOp, validUntil, validAfter, erc20Token, exchangeRate));
 
-        context = "";
-        if (erc20Token != address(0)) {
-            context =
-                abi.encode(userOp.sender, erc20Token, exchangeRate, userOp.maxFeePerGas, userOp.maxPriorityFeePerGas);
-        }
+        bytes32 hash = ECDSA.toEthSignedMessageHash(getHash(userOp, validUntil, validAfter, strategy));
 
-        // don't revert on signature failure: return SIG_VALIDATION_FAILED
+        // Don't revert on signature failure: return SIG_VALIDATION_FAILED with empty context
         if (owner() != ECDSA.recover(hash, signature)) {
-            return (context, Helpers._packValidationData(true, validUntil, validAfter));
+            return ("", Helpers._packValidationData(true, validUntil, validAfter));
         }
 
-        // If the parsePaymasterAndData was signed by the owner of the Paymaster
-        // return the context (empty if native token) and validity (validUntil, validAfter).
+        context = abi.encode(
+            userOp.sender,
+            strategy.paymasterMode,
+            strategy.erc20Token,
+            strategy.exchangeRate,
+            userOp.maxFeePerGas,
+            userOp.maxPriorityFeePerGas
+        );
+
+        // If the parsePaymasterAndData was signed by the owner of the paymaster
+        // return the context and validity (validUntil, validAfter).
         return (context, Helpers._packValidationData(false, validUntil, validAfter));
     }
 
     /*
-     * If everything worked fine, transfer the right amount of tokens from the sender (SCW) to the designated recipient
+     * For ERC20 modes, transfer the right amount of tokens from the sender to the designated recipient
      */
     function _postOp(PostOpMode mode, bytes calldata context, uint256 actualGasCost) internal override {
-        (address sender, IERC20 token, uint256 exchangeRate, uint256 maxFeePerGas, uint256 maxPriorityFeePerGas) =
-            abi.decode(context, (address, IERC20, uint256, uint256, uint256));
+        (
+            address sender,
+            Mode paymasterMode,
+            IERC20 token,
+            uint256 exchangeRate,
+            uint256 maxFeePerGas,
+            uint256 maxPriorityFeePerGas
+        ) = abi.decode(context, (address, Mode, IERC20, uint256, uint256, uint256));
 
-        uint256 opGasPrice;
-        unchecked {
-            if (maxFeePerGas == maxPriorityFeePerGas) {
-                opGasPrice = maxFeePerGas;
-            } else {
-                opGasPrice = Math.min(maxFeePerGas, maxPriorityFeePerGas + block.basefee);
+        if (paymasterMode == Mode.DynamicRate) {
+            uint256 opGasPrice;
+            unchecked {
+                if (maxFeePerGas == maxPriorityFeePerGas) {
+                    opGasPrice = maxFeePerGas;
+                } else {
+                    opGasPrice = Math.min(maxFeePerGas, maxPriorityFeePerGas + block.basefee);
+                }
             }
-        }
 
-        uint256 actualTokenCost = ((actualGasCost + (POST_OP_GAS * opGasPrice)) * exchangeRate) / 1e18;
-        if (mode != PostOpMode.postOpReverted) {
-            if (exchangeRate == 0) {
-                actualTokenCost = 1 ether;  // warning: this assumes the decimals of the ERC20 token is 18.
+            uint256 actualTokenCost = ((actualGasCost + (POST_OP_GAS * opGasPrice)) * exchangeRate) / 1e18;
+            if (mode != PostOpMode.postOpReverted) {
+                emit GasPaidInERC20(address(token), actualGasCost, actualTokenCost);
+                token.safeTransferFrom(sender, tokenRecipient, actualTokenCost);
             }
-            emit GasPaidInERC20(address(token), actualGasCost, actualTokenCost);
-            token.safeTransferFrom(sender, owner(), actualTokenCost);
+        } else if (paymasterMode == Mode.FixedRate) {
+            emit GasPaidInERC20(address(token), actualGasCost, exchangeRate);
+            token.safeTransferFrom(sender, tokenRecipient, exchangeRate);
         }
     }
 
     /**
-     * Parse paymasterAndData.
+     * Parse paymasterAndData
      * The "paymasterAndData" is expected to be the paymaster and a signature over the entire request params
      * paymasterAndData[:20]: address(this)
-     * paymasterAndData[20:SIGNATURE_OFFSET]: (validUntil, validAfter, erc20Token, exchangeRate) // 48+48+20+32
+     * paymasterAndData[20:SIGNATURE_OFFSET]: (validUntil, validAfter, strategy) // 20+48+48+64
      * paymasterAndData[SIGNATURE_OFFSET:]: signature
      */
     function parsePaymasterAndData(bytes calldata paymasterAndData)
         public
         pure
-        returns (
-            uint48 validUntil,
-            uint48 validAfter,
-            address erc20Token,
-            uint256 exchangeRate,
-            bytes calldata signature
-        )
+        returns (uint48 validUntil, uint48 validAfter, PolicyStrategy memory strategy, bytes calldata signature)
     {
-        (validUntil, validAfter, erc20Token, exchangeRate) =
-            abi.decode(paymasterAndData[VALID_PND_OFFSET:SIGNATURE_OFFSET], (uint48, uint48, address, uint256));
+        (validUntil, validAfter, strategy) =
+            abi.decode(paymasterAndData[VALID_PND_OFFSET:SIGNATURE_OFFSET], (uint48, uint48, PolicyStrategy));
         signature = paymasterAndData[SIGNATURE_OFFSET:];
+    }
+
+    /**
+     * Allows the owner of the paymaster to update the token recipient address
+     */
+    function updateTokenRecipient(address _newTokenRecipient) external onlyOwner {
+        if (_newTokenRecipient == address(0)) revert InvalidTokenRecipient();
+        emit TokenRecipientUpdated(tokenRecipient, _newTokenRecipient);
+        tokenRecipient = _newTokenRecipient;
     }
 }
