957bfa5d0fd75f6f71b3a384bc7763f0087f69f2
Eloi Manuel <eloi.manvie@gmail.com>
2023-09-22 18:44:32 +0200

    Almost final V2

--- contracts/interfaces/OpenfortErrorsAndEvents.sol
+++ contracts/interfaces/OpenfortErrorsAndEvents.sol
@@ -2,9 +2,18 @@
 pragma solidity ^0.8.19;
 
 interface OpenfortErrorsAndEvents {
-    /// @notice Error when an address parameter is 0.
-    error ZeroAddressNotAllowed();
+    /// @notice Error when a parameter is 0.
+    error ZeroValueNotAllowed();
 
     /// @notice Error when a function requires msg.value to be different than 0
     error MustSendNativeToken();
+
+    // Paymaster specifics
+
+    /**
+     * @notice Throws when trying to withdraw more than balance available
+     * @param amountRequired required balance
+     * @param currentBalance available balance
+     */
+    error InsufficientBalance(uint256 amountRequired, uint256 currentBalance);
 }
