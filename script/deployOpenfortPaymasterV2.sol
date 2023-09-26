ad17dd74c8943275cce9b76894970aaf19187fb4
Eloi Manuel <eloi.manvie@gmail.com>
2023-09-26 11:40:05 +0200

    Fix withdrawTo

---
+++ script/deployOpenfortPaymasterV2.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.19;
+
+import {Script, console} from "forge-std/Script.sol";
+import {IEntryPoint} from "lib/account-abstraction/contracts/interfaces/IEntryPoint.sol";
+import {OpenfortPaymasterV2} from "../contracts/paymaster/OpenfortPaymasterV2.sol";
+
+contract OpenfortPaymasterV2Deploy is Script {
+    uint256 internal deployPrivKey = vm.deriveKey(vm.envString("MNEMONIC_TESTNET"), 0);
+    // uint256 internal deployPrivKey = vm.envUint("PK");
+    address internal deployAddress = vm.addr(deployPrivKey);
+    IEntryPoint internal entryPoint = IEntryPoint((payable(vm.envAddress("ENTRY_POINT_ADDRESS"))));
+
+    function run() public {
+        bytes32 versionSalt = vm.envBytes32("VERSION_SALT");
+        vm.startBroadcast(deployPrivKey);
+
+        OpenfortPaymasterV2 openfortPaymaster = new OpenfortPaymasterV2{salt: versionSalt}(entryPoint, deployAddress);
+
+        entryPoint.depositTo{value: 1 ether}(address(openfortPaymaster));
+
+        vm.stopBroadcast();
+    }
+}
