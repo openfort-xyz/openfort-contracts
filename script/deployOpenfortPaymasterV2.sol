c2be5e7912862e1ac84d3363fdcb91d8ef276147
Eloi Manuel <eloi.manvie@gmail.com>
2023-09-27 09:34:04 +0200

    updated deploy scripts Openfort paymasters

--- script/deployOpenfortPaymasterV2.sol
+++ script/deployOpenfortPaymasterV2.sol
@@ -10,6 +10,7 @@ contract OpenfortPaymasterV2Deploy is Script {
     // uint256 internal deployPrivKey = vm.envUint("PK");
     address internal deployAddress = vm.addr(deployPrivKey);
     IEntryPoint internal entryPoint = IEntryPoint((payable(vm.envAddress("ENTRY_POINT_ADDRESS"))));
+    uint256 internal constant UNSTAKEDELAYSEC = 8600;
 
     function run() public {
         bytes32 versionSalt = vm.envBytes32("VERSION_SALT");
@@ -17,7 +18,8 @@ contract OpenfortPaymasterV2Deploy is Script {
 
         OpenfortPaymasterV2 openfortPaymaster = new OpenfortPaymasterV2{salt: versionSalt}(entryPoint, deployAddress);
 
-        entryPoint.depositTo{value: 1 ether}(address(openfortPaymaster));
+        entryPoint.depositTo{value: 0.5 ether}(address(openfortPaymaster));
+        entryPoint.addStake{value: 0.25 ether}(UNSTAKEDELAYSEC);
 
         vm.stopBroadcast();
     }
