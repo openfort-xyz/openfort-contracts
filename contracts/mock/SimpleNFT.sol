eab6cdc5700c270631c025e37faa59eb5c56e233
Eloi Manuel <eloi.manvie@gmail.com>
2023-11-02 16:09:33 +0100

    Adding SimpleNFt and update forge

---
+++ contracts/mock/SimpleNFT.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
+
+/* solhint-disable reason-string */
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+// A simple ERC721 contract
+contract SimpleNFT is ERC721 {
+    uint256 public tokenId;
+
+    constructor() ERC721("SimpleNFT", "SNFT") {}
+
+    // Anyone can mint an NFT for anyone
+    function mint(address _to) public {
+        _safeMint(_to, tokenId++);
+    }
+}
