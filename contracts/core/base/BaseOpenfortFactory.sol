05daae9df70179647b30d2574485b8d24264dc0b
Eloi Manuel <eloi.manvie@gmail.com>
2023-11-22 15:04:32 +0100

    Moved BaseOpenfortFactory logic, better modularity

--- contracts/core/base/BaseOpenfortFactory.sol
+++ contracts/core/base/BaseOpenfortFactory.sol
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity =0.8.19;
 
+import {Ownable2StepUpgradeable} from "@openzeppelin/contracts-upgradeable/access/Ownable2StepUpgradeable.sol";
+import {AddressUpgradeable} from "@openzeppelin/contracts-upgradeable/utils/AddressUpgradeable.sol";
+import {IEntryPoint} from "account-abstraction/interfaces/IEntryPoint.sol";
 import {IBaseOpenfortFactory} from "../../interfaces/IBaseOpenfortFactory.sol";
 
 /**
@@ -9,7 +12,7 @@ import {IBaseOpenfortFactory} from "../../interfaces/IBaseOpenfortFactory.sol";
  * It inherits from:
  *  - IBaseOpenfortFactory
  */
-abstract contract BaseOpenfortFactory is IBaseOpenfortFactory {
+abstract contract BaseOpenfortFactory is IBaseOpenfortFactory, Ownable2StepUpgradeable {
     address public entrypointContract;
     address public accountImplementation;
     uint256 public recoveryPeriod;
@@ -21,6 +24,7 @@ abstract contract BaseOpenfortFactory is IBaseOpenfortFactory {
     error InsecurePeriod();
 
     constructor(
+        address _owner,
         address _entrypoint,
         address _accountImplementation,
         uint256 _recoveryPeriod,
@@ -29,12 +33,16 @@ abstract contract BaseOpenfortFactory is IBaseOpenfortFactory {
         uint256 _lockPeriod,
         address _openfortGuardian
     ) {
-        if (_entrypoint == address(0) || _accountImplementation == address(0) || _openfortGuardian == address(0)) {
+        if (
+            _owner == address(0) || _entrypoint == address(0) || _accountImplementation == address(0)
+                || _openfortGuardian == address(0)
+        ) {
             revert ZeroAddressNotAllowed();
         }
         if (_lockPeriod < _recoveryPeriod || _recoveryPeriod < _securityPeriod + _securityWindow) {
             revert InsecurePeriod();
         }
+        _transferOwnership(_owner);
         entrypointContract = _entrypoint;
         accountImplementation = _accountImplementation;
         recoveryPeriod = _recoveryPeriod;
@@ -42,5 +50,26 @@ abstract contract BaseOpenfortFactory is IBaseOpenfortFactory {
         securityWindow = _securityWindow;
         lockPeriod = _lockPeriod;
         openfortGuardian = _openfortGuardian;
+    }
+
+    /**
+     * @dev {See IBaseOpenfortFactory}
+     */
+    function addStake(uint32 unstakeDelaySec) external payable onlyOwner {
+        IEntryPoint(entrypointContract).addStake{value: msg.value}(unstakeDelaySec);
+    }
+
+    /**
+     * @dev {See IBaseOpenfortFactory}
+     */
+    function unlockStake() external onlyOwner {
+        IEntryPoint(entrypointContract).unlockStake();
+    }
+
+    /**
+     * @dev {See IBaseOpenfortFactory}
+     */
+    function withdrawStake(address payable withdrawAddress) external onlyOwner {
+        IEntryPoint(entrypointContract).withdrawStake(withdrawAddress);
     }
 }
