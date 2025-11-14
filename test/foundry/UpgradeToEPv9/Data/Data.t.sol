9fb0be3d288b5115047f7d80fbdb152ccfaae705
0xkoiner <alexgray.bcn.esp@gmail.com>
2025-11-14 16:55:12 +0100

    (test) test_AfterDeploy

--- test/foundry/UpgradeToEPv9/Data/Data.t.sol
+++ test/foundry/UpgradeToEPv9/Data/Data.t.sol
@@ -4,8 +4,8 @@ pragma solidity ^0.8.19;
 
 import {Constants} from "./Constants.sol";
 import {Test} from "lib/forge-std/src/Test.sol";
-import {IUpgradeableOpenfortAccount} from "contracts/interfaces/IUpgradeableOpenfortAccount.sol";
 import {IEntryPoint as IEntryPointv6} from "lib/account-abstraction/contracts/core/EntryPoint.sol";
+import {UpgradeableOpenfortAccount} from "contracts/core/upgradeable/UpgradeableOpenfortAccount.sol";
 import {UpgradeableOpenfortFactory} from "contracts/core/upgradeable/UpgradeableOpenfortFactory.sol";
 import {IEntryPoint as IEntryPointv9} from "lib/account-abstraction-v09/contracts/core/EntryPoint.sol";
 
@@ -17,17 +17,22 @@ abstract contract Data is Test, Constants {
     IEntryPointv9 public entryPointV9;
 
     address internal _OpenfortAdmin;
-    uint259 internal _OpenfortAdminPK;
+    uint256 internal _OpenfortAdminPK;
 
     address internal _AccountOwner;
-    uint259 internal _AccountOwnerPK;
+    uint256 internal _AccountOwnerPK;
+
+    address internal _Guardian;
+    uint256 internal _GuardianPK;
 
     bytes32 public versionSalt;
 
     function setUp() public virtual {
         (_OpenfortAdmin, _OpenfortAdminPK) = makeAddrAndKey("_OpenfortAdmin");
         (_AccountOwner, _AccountOwnerPK) = makeAddrAndKey("_AccountOwner");
+        (_Guardian, _GuardianPK) = makeAddrAndKey("_Guardian");
 
-        versionSalt = vm.envBytes32("SALT_0001");
+        versionSalt = keccak256(abi.encodePacked("SALT_0001"));
+
     }
 }
--- test/foundry/UpgradeToEPv9/Deploy.t.sol
+++ test/foundry/UpgradeToEPv9/Deploy.t.sol
@@ -3,7 +3,7 @@
 pragma solidity ^0.8.19;
 
 import {Data} from "test/foundry/UpgradeToEPv9/Data/Data.t.sol";
-import {IUpgradeableOpenfortAccount} from "contracts/interfaces/IUpgradeableOpenfortAccount.sol";
+import {UpgradeableOpenfortAccount} from "contracts/core/upgradeable/UpgradeableOpenfortAccount.sol";
 import {UpgradeableOpenfortFactory} from "contracts/core/upgradeable/UpgradeableOpenfortFactory.sol";
 import {EntryPoint as EntryPointV6, IEntryPoint as IEntryPointv6} from "lib/account-abstraction/contracts/core/EntryPoint.sol";
 import {EntryPoint as EntryPointV9, IEntryPoint as IEntryPointv9} from "lib/account-abstraction-v09/contracts/core/EntryPoint.sol";
@@ -20,6 +20,33 @@ contract Deploy is Data {
         EntryPointV9 deployedEntryPointV9 = new EntryPointV9();
         vm.etch(ENTRY_POINT_V9, address(deployedEntryPointV9).code);
         vm.label(ENTRY_POINT_V9, "EntryPointV9");
-        entryPointV9 = IEntryPointv6(payable(ENTRY_POINT_V9));
+        entryPointV9 = IEntryPointv9(payable(ENTRY_POINT_V9));
+
+        vm.startPrank(_OpenfortAdmin);
+        upgradeableOpenfortAccountImpl = new UpgradeableOpenfortAccount{salt: versionSalt}();
+        openfortFactory = new UpgradeableOpenfortFactory{salt: versionSalt}(
+            _OpenfortAdmin,
+            address(entryPointV6),
+            address(upgradeableOpenfortAccountImpl),
+            RECOVERY_PERIOD,
+            SECURITY_PERIOD,
+            SECURITY_WINDOW,
+            LOCK_PERIOD,
+            _Guardian
+        );
+        vm.stopPrank();
+    }
+
+    function test_AfterDeploy() external {
+        assertEq(address(entryPointV6), ENTRY_POINT_V6);
+        assertEq(address(entryPointV9), ENTRY_POINT_V9);
+        assertEq(openfortFactory.owner(), _OpenfortAdmin);
+        assertEq(openfortFactory.initialGuardian(), _Guardian);
+        assertEq(openfortFactory.implementation(), address(upgradeableOpenfortAccountImpl));
+        assertEq(openfortFactory.entrypointContract(), ENTRY_POINT_V6);
+        assertEq(openfortFactory.lockPeriod(), LOCK_PERIOD);
+        assertEq(openfortFactory.recoveryPeriod(), RECOVERY_PERIOD);
+        assertEq(openfortFactory.securityPeriod(), SECURITY_PERIOD);
+        assertEq(openfortFactory.securityWindow(), SECURITY_WINDOW);
     }
 }
