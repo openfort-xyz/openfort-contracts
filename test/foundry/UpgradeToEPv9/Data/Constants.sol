1ef14a5dfefa14027d7eb76d3a7f3ea2207a9b82
0xkoiner <alexgray.bcn.esp@gmail.com>
2025-11-14 16:14:00 +0100

    (test) UpgradeToEPv9

---
+++ test/foundry/UpgradeToEPv9/Data/Constants.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.29;
+
+abstract contract Constants {
+    address constant internal ENTRY_POINT_V6 = 0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789;
+    address constant internal ENTRY_POINT_V9 = 0x43370900c8de573dB349BEd8DD53b4Ebd3Cce709;
+
+    uint256 public constant RECOVERY_PERIOD = 2 days;
+    uint256 public constant SECURITY_PERIOD = 1.5 days;
+    uint256 public constant SECURITY_WINDOW = 0.5 days;
+    uint256 public constant LOCK_PERIOD = 5 days;
+}
