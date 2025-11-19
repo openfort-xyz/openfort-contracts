30721cba9886b9ae82924f46e6cb0b7259a5512e
0xkoiner <alexgray.bcn.esp@gmail.com>
2025-11-19 18:12:13 +0100

    (test) SessionKeyTest::test_SendERC20ToAnyAddressWithMKAA

--- test/foundry/UpgradeToEPv9/tests/SessionKeyTest.t.sol
+++ test/foundry/UpgradeToEPv9/tests/SessionKeyTest.t.sol
@@ -273,6 +273,34 @@ contract SessionKeyTest is Deploy {
         _assertBalances(address(0xbabe), false, 0.01 ether * 5);
     }
 
+    function test_SendERC20ToAnyAddressWithMKAA() external {
+        _registerKeyAA(true);
+        _assertRegistratedKey(SK, true);
+        _mint(address(_RandomOwnerSC), 100 ether);
+        _assertBalancesERC20(address(0xdeadbabe), true, 0.01 ether);
+    
+        PackedUserOperation memory userOp;
+        (, userOp) = _getFreshUserOp(address(_RandomOwnerSC));
+
+        bytes memory callData = abi.encodeWithSignature("transfer(address,uint256)", address(0xdeadbabe), 5 ether);
+
+        userOp = _populateUserOpV9(
+            userOp,
+            _createExecuteCall(address(erc20), 0, callData),
+            _packAccountGasLimits(400_000, 600_000),
+            800_000,
+            _packGasFees(15 gwei, 80 gwei),
+            hex""
+        );
+
+        bytes32 userOpHash = _getUserOpHashV9(userOp);
+
+        userOp.signature = _signUserOp(userOpHash, SK_PK);
+
+        _relayUserOpV9(userOp);
+        _assertBalancesERC20(address(0xdeadbabe), false, 5 ether);
+    }
+
     function test_SendERC20ToAnyAddressWithSKAA() external {
         _registerKeyAA(false);
         _assertRegistratedKey(SK, false); 
