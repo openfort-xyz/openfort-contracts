2e497b01be6f13d561299d02eaba7615bd6f60a1
0xkoiner <alexgray.bcn.esp@gmail.com>
2025-11-20 17:43:59 +0100

    (test) RevertsOwnershipTest::test_revert_transferOwnership_notOwner::test_revert_transferOwnership_accountLocked

---
+++ test/foundry/UpgradeToEPv9/tests/Reverts/RevertsOwnershipTest.t.sol
@@ -0,0 +1,90 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.29;
+
+import {PackedUserOperation} from "lib/account-abstraction-v09/contracts/interfaces/PackedUserOperation.sol";
+import {SocialRecoveryHelper} from "test/foundry/UpgradeToEPv9/helpers/SocialRecoveryHelper.t.sol";
+import {
+    UpgradeableOpenfortAccount as UpgradeableOpenfortAccountV9
+} from "contracts/coreV9/upgradeable/UpgradeableOpenfortAccount.sol";
+
+contract RevertsOwnershipTest is SocialRecoveryHelper {
+    address internal _RandomOwner;
+    uint256 internal _RandomOwnerPK;
+    bytes32 internal _RandomOwnerSalt;
+    UpgradeableOpenfortAccountV9 internal _RandomOwnerSC;
+
+    address internal _NewOwner;
+    address internal _Attacker;
+
+    RandomOwner internal randomOwner;
+
+    error AccountLocked();
+    error GuardianCannotBeOwner();
+    error NoOngoingRecovery();
+
+    function setUp() public override {
+        super.setUp();
+        (_RandomOwner, _RandomOwnerPK) = makeAddrAndKey("_RandomOwner");
+        _NewOwner = makeAddr("_NewOwner");
+        _Attacker = makeAddr("_Attacker");
+        (_RecoveryOwner, _RecoveryOwnerPK) = makeAddrAndKey("_RecoveryOwner");
+        _deal(_RandomOwner, 5 ether);
+        _RandomOwnerSalt = keccak256(abi.encodePacked("0xbebe_0001"));
+        _createAccountV9();
+        _deal(address(_RandomOwnerSC), 5 ether);
+        randomOwner = RandomOwner(_RandomOwner, _RandomOwnerSC);
+    }
+
+    function test_revert_transferOwnership_notOwner() external {
+        vm.prank(_Attacker);
+        vm.expectRevert("Ownable: caller is not the owner");
+        _RandomOwnerSC.transferOwnership(_NewOwner);
+    }
+
+    function test_revert_transferOwnership_accountLocked() external createGuardians(1) {
+        _executeGuardianAction(randomOwner, GuardianAction.PROPOSE, 1);
+        _executeGuardianAction(randomOwner, GuardianAction.CONFIRM_PROPOSAL, 1);
+
+        vm.prank(_Guardians[0]);
+        _RandomOwnerSC.lock();
+
+        vm.prank(_RandomOwner);
+        vm.expectRevert(AccountLocked.selector);
+        _RandomOwnerSC.transferOwnership(_NewOwner);
+    }
+    
+    function _createAccountV9() internal {
+        address _RandomOwnerSCAddr = openfortFactoryV9.getAddressWithNonce(_RandomOwner, _RandomOwnerSalt);
+        _RandomOwnerSC = UpgradeableOpenfortAccountV9(payable(_RandomOwnerSCAddr));
+        _depositTo(_RandomOwner, address(_RandomOwnerSC), EP_Version.V9);
+        _sendAssetsToSC(_RandomOwner, address(_RandomOwnerSC));
+
+        PackedUserOperation memory userOp;
+        (, userOp) = _getFreshUserOp(address(_RandomOwnerSC));
+
+        bytes memory callData = hex"";
+        userOp = _populateUserOpV9(
+            userOp, callData, _packAccountGasLimits(400_000, 600_000), 800_000, _packGasFees(15 gwei, 80 gwei), hex""
+        );
+
+        bytes memory initCode = abi.encodeWithSignature(
+            "createAccountWithNonce(address,bytes32,bool)", _RandomOwner, _RandomOwnerSalt, false
+        );
+        userOp.initCode = abi.encodePacked(address(openfortFactoryV9), initCode);
+
+        bytes32 userOpHash = _getUserOpHashV9(userOp);
+
+        userOp.signature = _signUserOp(userOpHash, _RandomOwnerPK);
+
+        _relayUserOpV9(userOp);
+    }
+
+    function _relayUserOpV9(PackedUserOperation memory _userOp) internal {
+        PackedUserOperation[] memory ops = new PackedUserOperation[](1);
+        ops[0] = _userOp;
+
+        vm.prank(_OpenfortAdmin, _OpenfortAdmin);
+        entryPointV9.handleOps(ops, payable(_OpenfortAdmin));
+    }
+}
