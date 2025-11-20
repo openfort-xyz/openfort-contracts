ed096782497e1d4c6d81b070db7911cd03a35af8
0xkoiner <alexgray.bcn.esp@gmail.com>
2025-11-20 16:59:08 +0100

    (test) RevertsExecutionTest::test_revert_executeBatchAA_oneCallFails::test_revert_executeBatchAA_insufficientBalance

--- test/foundry/UpgradeToEPv9/tests/Reverts/RevertsExecutionTest.t.sol
+++ test/foundry/UpgradeToEPv9/tests/Reverts/RevertsExecutionTest.t.sol
@@ -355,6 +355,72 @@ contract RevertsExecutionTest is Deploy {
 
         _relayUserOpV9(userOp);
     }
+
+
+    function test_revert_executeBatchAA_oneCallFails() external {
+        address[] memory targets = new address[](3);
+        targets[0] = address(0xbabe);
+        targets[1] = address(revertingContract);
+        targets[2] = address(0xbeef);
+
+        uint256[] memory values = new uint256[](3);
+        values[0] = 0;
+        values[1] = 0;
+        values[2] = 0;
+
+        bytes[] memory datas = new bytes[](3);
+        datas[0] = hex"";
+        datas[1] = abi.encodeWithSelector(RevertingContract.revertWithCustomError.selector);
+        datas[2] = hex"";
+
+        PackedUserOperation memory userOp;
+        (, userOp) = _getFreshUserOp(address(_RandomOwnerSC));
+
+        userOp = _populateUserOpV9(
+            userOp,
+            _createExecuteBatchCall(targets, values, datas),
+            _packAccountGasLimits(400_000, 600_000),
+            800_000,
+            _packGasFees(15 gwei, 80 gwei),
+            hex""
+        );
+
+        bytes32 userOpHash = _getUserOpHashV9(userOp);
+        userOp.signature = _signUserOp(userOpHash, _RandomOwnerPK);
+
+        _relayUserOpV9(userOp);
+    }
+
+    function test_revert_executeBatchAA_insufficientBalance() external {
+        address[] memory targets = new address[](2);
+        targets[0] = address(0xbabe);
+        targets[1] = address(0xdead);
+
+        uint256[] memory values = new uint256[](2);
+        values[0] = 100 ether;
+        values[1] = 100 ether;
+
+        bytes[] memory datas = new bytes[](2);
+        datas[0] = hex"";
+        datas[1] = hex"";
+
+        PackedUserOperation memory userOp;
+        (, userOp) = _getFreshUserOp(address(_RandomOwnerSC));
+
+        userOp = _populateUserOpV9(
+            userOp,
+            _createExecuteBatchCall(targets, values, datas),
+            _packAccountGasLimits(400_000, 600_000),
+            800_000,
+            _packGasFees(15 gwei, 80 gwei),
+            hex""
+        );
+
+        bytes32 userOpHash = _getUserOpHashV9(userOp);
+        userOp.signature = _signUserOp(userOpHash, _RandomOwnerPK);
+
+        _relayUserOpV9(userOp);
+    }
     
     function _createAccountV9() internal {
         address _RandomOwnerSCAddr = openfortFactoryV9.getAddressWithNonce(_RandomOwner, _RandomOwnerSalt);
