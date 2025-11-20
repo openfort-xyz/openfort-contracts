93ee3234029a0a3c8fd4830b9b17443a527f1f43
0xkoiner <alexgray.bcn.esp@gmail.com>
2025-11-20 16:29:05 +0100

    (test) RevertsExecutionTest::test_revert_execute_notOwnerOrEntrypoint

---
+++ test/foundry/UpgradeToEPv9/tests/Reverts/RevertsExecutionTest.t.sol
@@ -0,0 +1,88 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.29;
+
+import {Deploy} from "test/foundry/UpgradeToEPv9/Deploy.t.sol";
+import {PackedUserOperation} from "lib/account-abstraction-v09/contracts/interfaces/PackedUserOperation.sol";
+import {
+    UpgradeableOpenfortAccount as UpgradeableOpenfortAccountV9
+} from "contracts/coreV9/upgradeable/UpgradeableOpenfortAccount.sol";
+
+contract RevertingContract {
+    error CustomRevertError();
+
+    function revertWithCustomError() external pure {
+        revert CustomRevertError();
+    }
+
+    function revertWithMessage() external pure {
+        revert("Target reverted");
+    }
+}
+
+contract RevertsExecutionTest is Deploy {
+    address internal _RandomOwner;
+    uint256 internal _RandomOwnerPK;
+    bytes32 internal _RandomOwnerSalt;
+    UpgradeableOpenfortAccountV9 internal _RandomOwnerSC;
+
+    address internal _Attacker;
+    uint256 internal _AttackerPK;
+
+    RevertingContract internal revertingContract;
+
+    error NotOwnerOrEntrypoint();
+    error InvalidParameterLength();
+
+    function setUp() public override {
+        super.setUp();
+        (_RandomOwner, _RandomOwnerPK) = makeAddrAndKey("_RandomOwner");
+        (_Attacker, _AttackerPK) = makeAddrAndKey("_Attacker");
+        _deal(_RandomOwner, 5 ether);
+        _RandomOwnerSalt = keccak256(abi.encodePacked("0xbebe_0001"));
+        _createAccountV9();
+        _deal(address(_RandomOwnerSC), 5 ether);
+
+        revertingContract = new RevertingContract();
+    }
+
+    function test_revert_execute_notOwnerOrEntrypoint() external {
+        vm.prank(_Attacker);
+        vm.expectRevert(NotOwnerOrEntrypoint.selector);
+        _RandomOwnerSC.execute(address(0xbabe), 0.01 ether, hex"");
+    }
+    
+    function _createAccountV9() internal {
+        address _RandomOwnerSCAddr = openfortFactoryV9.getAddressWithNonce(_RandomOwner, _RandomOwnerSalt);
+        _RandomOwnerSC = UpgradeableOpenfortAccountV9(payable(_RandomOwnerSCAddr));
+        _depositTo(_RandomOwner, address(_RandomOwnerSC), EP_Version.V9);
+        _sendAssetsToSC(_RandomOwner, address(_RandomOwnerSC));
+
+        PackedUserOperation memory userOp;
+        (, userOp) = _getFreshUserOp(address(_RandomOwnerSC));
+
+        bytes memory callData = hex"";
+        userOp = _populateUserOpV9(
+            userOp, callData, _packAccountGasLimits(400_000, 600_000), 800_000, _packGasFees(15 gwei, 80 gwei), hex""
+        );
+
+        bytes memory initCode = abi.encodeWithSignature(
+            "createAccountWithNonce(address,bytes32,bool)", _RandomOwner, _RandomOwnerSalt, false
+        );
+        userOp.initCode = abi.encodePacked(address(openfortFactoryV9), initCode);
+
+        bytes32 userOpHash = _getUserOpHashV9(userOp);
+
+        userOp.signature = _signUserOp(userOpHash, _RandomOwnerPK);
+
+        _relayUserOpV9(userOp);
+    }
+
+    function _relayUserOpV9(PackedUserOperation memory _userOp) internal {
+        PackedUserOperation[] memory ops = new PackedUserOperation[](1);
+        ops[0] = _userOp;
+
+        vm.prank(_OpenfortAdmin, _OpenfortAdmin);
+        entryPointV9.handleOps(ops, payable(_OpenfortAdmin));
+    }
+}
